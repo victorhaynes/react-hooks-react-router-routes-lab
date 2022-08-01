50ebe782e1762a18fd3584304aaaf4600e6d6d82
Victor Haynes <haynesvic@gmail.com>
2022-07-31 19:53:31 -0500

    commit tree b7135fac83fbaeb699349bb0ce2f96764bbb8840

--- src/components/Actors.js
+++ src/components/Actors.js
@@ -2,7 +2,18 @@ import React from "react";
 import { actors } from "../data";
 
 function Actors() {
-  return <div>{/*{code here}*/}</div>;
-}
+  return (
+    <div>
+      <h1>Actors Page</h1>
+      {actors.map((actor)=>
+        <div key={actor.name}>
+          {actor.name}
+          <ul>Movies:
+            {actor.movies.map((movie)=> <li key ={movie}>{movie}</li>)}
+          </ul>
+        </div>
+      )}
+    </div>
+    );}
 
 export default Actors;
--- src/components/Movies.js
+++ src/components/Movies.js
@@ -1,8 +1,22 @@
 import React from "react";
 import { movies } from "../data";
 
+
 function Movies() {
-  return <div>{/*{code here}*/}</div>;
+  return (
+  <div>
+    <h1>Movies Page</h1>
+    {movies.map( (movie) =>
+      <div key={movie.title}>
+        Title: {movie.title}{" "}
+        Time: {movie.time}
+        <ul>Genres:
+          {movie.genres.map( (genre) => <li key={genre}>{genre}</li>)}
+        </ul>
+      </div>
+      )}
+  </div>
+  );
 }
 
 export default Movies;
